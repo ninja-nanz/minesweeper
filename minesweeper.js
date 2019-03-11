ed00bd994c8082ad9d630617525ed3e663b282bd
Nanz Nair <nanz89@gmail.com>
2019-03-11 17:42:37 +1300

    Win conditions

--- minesweeper.js
+++ minesweeper.js
@@ -13,8 +13,11 @@ var board = {
 function startGame () {
   for (var i=0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]) // assigning minecount to the surroundingMines property 
-  }
-  
+  };
+
+  document.addEventListener("click", checkForWin);
+  document.addEventListener("contextmenu", checkForWin)
+
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -26,13 +29,25 @@ function startGame () {
 // 1. Are all of the cells that are NOT mines visible?
 // 2. Are all of the mines marked?
 function checkForWin () {
-
-  // You can use this function call to declare a winner (once you've
-  // detected that they've won, that is!)
-  //   lib.displayMessage('You win!')
+  for (var i=0; i < board.cells.length; i++) {
+    if (board.cells[i].isMine == true && board.cells[i].isMarked == false ) {
+      return;
+    }
+    else if (board.cells[i].isMine == false && board.cells[i].hidden == true) {
+      return;
+    } 
+  }
+  
+  lib.displayMessage('You win! Woo!')
+    
 }
 
 
+//   board.map(cell => cell.isMine === true &&  board.cells[i].isMarked === true)
+  
+
+ // You can use this function call to declare a winner (once you've
+  // detected that they've won, that is!)
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
@@ -43,8 +58,11 @@ function checkForWin () {
 // It will return cell objects in an array. You should loop through 
 // them, counting the number of times `cell.isMine` is true.
 
-function countSurroundingMines (cell) {
-  var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);       // get the array of surrounding cells
-  return surroundingCells.filter(cell => cell.isMine === true).length      // filter to only show the ones that have mines, give length of array after filtering
-} 
+// 1- get the array of surrounding cells
+// 2- filter to only show the ones that have mines, give length of array after filtering
 
+
+function countSurroundingMines (cell) {
+  var surroundingCells = lib.getSurroundingCells(cell.row, cell.col);      
+  return surroundingCells.filter(cell => cell.isMine === true).length
+}
